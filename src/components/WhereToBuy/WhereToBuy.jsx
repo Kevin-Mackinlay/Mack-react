0bd35fa0927f8684d99aa14603804278cb584c45
Mackinlay Kevin <kemack83@gmail.com>
2024-08-26 19:31:54 -0300

    finished whereToBuy component and applied frame-motion

--- src/components/WhereToBuy/WhereToBuy.jsx
+++ src/components/WhereToBuy/WhereToBuy.jsx
@@ -1,13 +1,36 @@
+import worldMap from '../../assets/worldmap.png';
+import { motion } from 'framer-motion';
+
 const WhereToBuy = () => {
   return (
     <div className="container my-36">
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
         {/*  form section  */}
-        <div></div>
+        <div className="space-y-8 ">
+          <motion.h1 initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: 0.2 }} className="text-4xl font-bold text-darkGray font-serif">
+            Buy from anywhere
+          </motion.h1>
+          <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.2 }} className="flex items-center gap-4">
+            <input type="text" placeholder="Name" className="input-style w-full lg:w-[170px]" />
+            <input type="email" placeholder="Email" className="input-style w-full lg:w-[170px]" />
+          </motion.div>
+          <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.2 }} className="flex items-center gap-4">
+            <input type="text" placeholder="Country" className="input-style w-full lg:w-[170px]" />
+            <input type="text" placeholder="Zipcode" className="input-style w-full lg:w-[170px]" />
+          </motion.div>
+          <motion.button initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.2 }} className="primary-btn  w-full">
+            Order Now
+          </motion.button>
+        </div>
         {/* world map  section  */}
         <div className="col-span-2">
-            <img src="" alt=""/>
-
+          <motion.img 
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{type: 'spring', stiffness: 100, damping: 10, delay: 1 }}
+          
+          
+          src={worldMap} alt="" className="w-full sm:w-[500px] mx-auto" />
         </div>
       </div>
     </div>
