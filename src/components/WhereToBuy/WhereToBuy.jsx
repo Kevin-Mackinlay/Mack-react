10b0ca744947306824611f7ae35921e4ac152f2e
Mackinlay Kevin <kemack83@gmail.com>
2024-08-26 11:26:41 -0300

    whereToBuy component, working on it

--- src/components/WhereToBuy/WhereToBuy.jsx
+++ src/components/WhereToBuy/WhereToBuy.jsx
@@ -1,13 +1,17 @@
-
 const WhereToBuy = () => {
   return (
-    <div>
-        
-        <h1 className="text-2xl font-bold text-center">Where To Buy</h1>
-        
-        
-       </div>
-  )
-}
+    <div className="container my-36">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
+        {/*  form section  */}
+        <div></div>
+        {/* world map  section  */}
+        <div className="col-span-2">
+            <img src="" alt=""/>
 
-export default WhereToBuy
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WhereToBuy;
