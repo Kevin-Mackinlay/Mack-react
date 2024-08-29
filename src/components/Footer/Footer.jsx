0ef9331ea49efa44b3a6a56308f0e78c340a05cd
Mackinlay Kevin <kemack83@gmail.com>
2024-08-29 16:27:44 -0300

    webpage finished

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,103 @@
-import React from 'react'
+import { FaFacebook, FaGoogle, FaInstagram, FaPhone, FaTelegram } from 'react-icons/fa';
+import { FaMapLocation } from 'react-icons/fa6';
+import CreditCards from '../../assets/website/credit-cards.webp';
+import { motion } from 'framer-motion';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-gradient-to-r from-primary to-primaryDark pt-12 pb-8 text-white">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 ">
+          {/* {company details section */}
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 100,
+            }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.6 }}
+            className="space-y-6"
+          >
+            <h1 className="text-3xl font-bold uppercase">Smart Sync</h1>
+            <p className="text-sm max-w-[300px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolores quia doloremque.</p>
 
-export default Footer
+            <div>
+              <p className="flex items-center gap-2">
+                <FaPhone />
+                <span className="ml-2">+54 2920409186</span>
+              </p>
+              <p className="flex items-center gap-2">
+                {' '}
+                <FaMapLocation />
+                Viedma, Rio Negro, Argentina.
+              </p>
+            </div>
+          </motion.div>
+
+          {/* {Footer Links section */}
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 100,
+            }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.6, duration: 0.6 }}
+            className="space-y-6"
+          >
+            <h1 className="text-3xl font-bold">Our Links</h1>
+            <div className=" grid grid-cols-2  gap-3 ">
+              {/* {first column section */}
+              <div className="">
+                <ul className="space-y-2">
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact us</li>
+                  <li>Privacy Policy</li>
+                </ul>
+              </div>
+              {/* {second column section */}
+              <div className="">
+                <ul className="space-y-2">
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Contact us</li>
+                  <li>Privacy Policy</li>
+                </ul>
+              </div>
+            </div>
+          </motion.div>
+          {/* {Social Links section */}
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 100,
+            }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.9, duration: 0.6 }}
+            className="space-y-6"
+          >
+            <h1 className="text-3xl font-bold">Follow Us</h1>
+            <div className="flex items-center gap-3">
+              <FaFacebook className="text-3xl hover:scale-105 duration-300" />
+              <FaInstagram className="text-3xl hover:scale-105 duration-300" />
+              <FaTelegram className="text-3xl hover:scale-105 duration-300" />
+              <FaGoogle className="text-3xl hover:scale-105 duration-300" />
+            </div>
+            <div className="">
+              <h1 className="text-xl font-semibold mb-2">Payment Methods</h1>
+              <img src={CreditCards} alt="credit cards" className="w-[80%]" />
+            </div>
+          </motion.div>
+        </div>
+
+        {/* {copyright section */}
+        <p className="text-center text-sm mt-8 pt-8 border-t-2">Copyright &copy; {new Date().getFullYear()} All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,13 +29,11 @@ const [sidebar, setSidebar] = useState(false);
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[850px]">
             {/* text content section */}
             <div className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
-              <motion.h1 initial={{ opacity: 0, y: -100 }}
-               animate={{ opacity: 1, y: 0 }}
-                transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1 }} className="text-7xl font-bold leading-tight ml-14">
+              <motion.h1 initial={{ opacity: 0, y: -100 }} whileInView={{ opacity: 1, y: 1 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1 }} className="text-7xl font-bold leading-tight ml-14">
                 Apple
               </motion.h1>
 
-              <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }} className="relative ">
+              <motion.div initial={{ opacity: 0, y: 100 }} whileInView={{ opacity: 1, y: 1 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }} className="relative ">
                 <div className="relative z-10 space-y-4">
                   <h2 className="text-2xl font-semibold">More powerful</h2>
                   <h2 className="text-sm opacity-55 leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia rem, a explicabo sint numquam laboriosam consequatur sapiente dolore. </h2>
@@ -46,41 +44,36 @@ const [sidebar, setSidebar] = useState(false);
             </div>
             {/* Hero Image section */}
             <div className="relative">
-              <motion.img initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.4 }} src={iphone1} alt="iphone" className="relative z-40 h-[400px] md:h-[500px] top-20 img-shadow" />
+              <motion.img initial={{ opacity: 0, scale: 0 }} whileInView={{ opacity: 1, scale: 1 }}
+              viewport={{ once: true }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.4 }} src={iphone1} alt="iphone" className="relative z-40 h-[400px] md:h-[500px] top-20 img-shadow" />
 
               {/* {big text area here} */}
-              <motion.div initial={{ opacity: 0, y: -100 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }} className="absolute -top-10 left-[400px] z-[1] ">
+              <motion.div initial={{ opacity: 0, y: -100 }} whileInView={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }} className="absolute -top-10 left-[400px] z-[1] ">
                 <h1 className="text-[140px] scale-150 font-bold text-darkGray/40 leading-none ">Apple</h1>
               </motion.div>
             </div>
 
             {/* third div section */}
-            <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }} className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
-              <h1 className=" opacity-0 text-7xl font-bold leading-tight ml-14">Apple</h1>
+            <div className=" hidden lg:block ">
+              <motion.div initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 1.2 }} className="text-lightOrange mt-[100px] md:mt-0 p-4 space-y-28">
+                <h1 className=" opacity-0 text-7xl font-bold leading-tight ml-14">Apple</h1>
 
-              <div className="relative ">
-                <div className="relative z-10 space-y-4">
-                  <h2 className="text-2xl font-semibold ">New Iphone</h2>
-                  <h2 className="text-sm opacity-55 leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia rem, a explicabo sint. Non mollitia rem, a explicabo sint. </h2>
+                <div className="relative ">
+                  <div className="relative z-10 space-y-4">
+                    <h2 className="text-2xl font-semibold ">New Iphone</h2>
+                    <h2 className="text-sm opacity-55 leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non mollitia rem, a explicabo sint. Non mollitia rem, a explicabo sint. </h2>
+                  </div>
+
+                  <div className="absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50"></div>
                 </div>
-
-                <div className="absolute -top-6 -right-10 w-[250px] h-[190px] bg-darkGray/50"></div>
-              </div>
-            </motion.div>
-            <div></div>
+              </motion.div>
+              <div></div>
+            </div>
           </div>
         </div>
         {/* {sidebar Menu section} */}
         {sidebar && (
-          <motion.div
-          initial={{ x: '100%' }}
-          whileInView={{x:0}}
-          
-          
-          
-          
-          
-          className="absolute top-0 right-0 w-[140px] h-full bg-gradient-to-b from-primary/80 to-primaryDark/80 backdrop-blur-sm z-10">
+          <motion.div initial={{ x: '100%' }} whileInView={{ x: 0 }} className="absolute top-0 right-0 w-[140px] h-full bg-gradient-to-b from-primary/80 to-primaryDark/80 backdrop-blur-sm z-10">
             <div className="w-full h-full flex justify-center items-center">
               <div className="flex flex-col justify-center items-center gap-6 text-white">
                 {/* {line} */}
